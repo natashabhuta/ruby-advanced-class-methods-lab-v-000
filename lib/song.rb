a5864498a9179e9707f4e48a17c65593b1f32808
natashabhuta <natasha.bhuta@gmail.com>
2018-08-18 02:15:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,9 @@ class Song
   song_array = filename.split(" - ")
     song_array[1] = song_array[1].chomp(".mp3")
   song = self.new
-  
+  song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
   binding.pry
   
 
